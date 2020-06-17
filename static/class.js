93bf89474ccf33c5f2a5b27f0bb8586a11b6ea39
Rei Meytal <reimeytal52@gmail.com>
2020-06-17 15:28:37 +0300

    Added tiebreaker if statement. Started working on removePoint function

--- static/class.js
+++ static/class.js
@@ -4,34 +4,49 @@ class Player{
     this.points = "0";
     this.games = 0;
     this.sets = 0;
-  
   }
   //Add set check and win check
-  addPoint(player2){
-    if(this.points == "0"){
-      this.points = "15";
-    } else if(this.points == "15"){
-      this.points = "30";
-    } else if(this.points  == "30"){
-      this.points == "40";
-    } else if(this.points == "40"){
-      if (player2.points != "40"){
+  addPoint(player2, tiebreaker){
+    if(!tiebreaker){
+      if(this.points == "0"){
+        this.points = "15";
+      } else if(this.points == "15"){
+        this.points = "30";
+      } else if(this.points  == "30"){
+        this.points == "40";
+      } else if(this.points == "40"){
+        if (player2.points != "40"){
+          this.points = "0";
+          this.games++;
+        } else if(player2.points == "40"){
+          this.points = "Adv";
+          player2.points = "40";
+        } else if(player2.points == "Adv"){
+          this.points = "40";
+          player2.points = "40";
+        }
+      } else if(this.points == "Adv"){
         this.points = "0";
         this.games++;
-      } else if(player2.points == "40"){
-        this.points = "Adv";
-        player2.points = "40";
-      } else if(player2.points == "Adv"){
-        this.points = "40";
-        player2.points = "40";
       }
-    } else if(this.points == "Adv"){
-      this.points = "0";
-      this.games++;
+    } else{
+      //Add tiebreaker here
     }
   }
   //Add remove point
-  removePoint(){
-    return null;
+  removePoint(tiebreaker){
+    if(!tiebreaker){
+      if(this.points == "Adv"){
+        this.points = "40";
+      } else if(this.points == "40"){
+        this.points = "30";
+      } else if(this.points == "30"){
+        this.points = "15";
+      } else if(this.points == "15"){
+        this.points = "0";
+      }
+    } else{
+      //handle tiebreaker here
+    }
   }
 }
