efb8b60269a05c8e6964cf442877a2a375e653de
Rei Meytal <reimeytal52@gmail.com>
2020-06-18 18:28:31 +0300

    Started working on main game page

--- static/main.js
+++ static/main.js
@@ -1,3 +1,7 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-
-});
+function initialize_game(player1Name,  player2Name){
+  player1  = new Player(player1Name);
+  player2 = new Player(player2Name);
+  document.addEventListener("DOMContentLoaded", ()=>{
+    
+  });
+}
