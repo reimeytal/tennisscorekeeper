6257132cfa2f78d8f469b7dafa16565fbd4cc916
reimeytal <rei12.meytal11@gmail.com>
2020-06-19 18:27:28 +0300

    Made scoreboard update upon receiving new data

--- static/main.js
+++ static/main.js
@@ -6,8 +6,8 @@ function initialize_game(player1Name,  player2Name){
   var player2 = new Player(player2Name);
   document.addEventListener("DOMContentLoaded", ()=>{
     const sock= io.connect(location.protocol+"//"+document.domain+":"+location.port);
-    addButtons = document.querySelectorAll("#player_add");
-    removeButtons = document.querySelectorAll("#player_remove");
+    addButtons = document.querySelectorAll(".player_add");
+    removeButtons = document.querySelectorAll(".player_remove");
     let i;
     for(i=0; i<addButtons.length; i++){
       let button = addButtons[i];
@@ -31,5 +31,38 @@ function initialize_game(player1Name,  player2Name){
         sock.emit("host-update", score(player1.getScore(), player2.getScore()));
       }
     }
+    sock.on("update") = (data) => {
+      player1.setScore(data["player1_score"]);
+      player2.setScore(data["player2_score"]);
+      //Player 1 text for loop
+      p1 = document.querySelectorAll(".player1");
+      for(i=0; i<p1.length; i++){
+        let x = p1[i];
+        if(x.dataset.type == "sets"){
+          x.innerHTML = player1.sets;
+        } else if(x.dataset.type == "games"){
+          x.innerHTML = playeer1.games;
+        } else if(x.dataset.type == "points"){
+          x.innerHTML = player1.points;
+        }
+      }
+      p2 = document.querySelectorAll(".player2");
+      for(i=0; i<p2.length; i++){
+        let x = p2[i];
+        if(x.dataset.type == "sets"){
+          x.innerHTML = player2.sets;
+        } else if(x.dataset.type == "games"){
+          x.innerHTML = player2.games;
+        } else if(x.dataset.type == "points"){
+          x.innerHTML = playere2.points;
+        }
+      }
+
+    }
   });
 }
+/*
+document.addEventListener("beforeunload", () => {
+  //Create unload function here
+});
+*/
